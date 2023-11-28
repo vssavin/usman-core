e9daa3f99e8e4cf4a8fe81976a06bb588c56385c
vssavin <savin1801@gmail.com>
2023-11-28 14:13:11 +0300

    added classes to store URL permissions

---
+++ usman-core/src/main/java/com/github/vssavin/usmancore/config/AuthorizedUrlPermission.java
@@ -0,0 +1,54 @@
+package com.github.vssavin.usmancore.config;
+
+import java.util.Arrays;
+
+/**
+ * Immutable class to store URL permissions.
+ *
+ * @author vssavin on 28.11.2023.
+ */
+public class AuthorizedUrlPermission {
+
+    private static final String DEFAULT_HTTP_METHOD = "GET";
+
+    private final String url;
+
+    private final String httpMethod;
+
+    private final String[] roles;
+
+    public AuthorizedUrlPermission(String url, String httpMethod, Permission permission) {
+        this.url = url;
+        this.httpMethod = httpMethod;
+        this.roles = permission.getRoles();
+    }
+
+    public AuthorizedUrlPermission(String url, Permission permission) {
+        this.url = url;
+        this.httpMethod = DEFAULT_HTTP_METHOD;
+        this.roles = permission.getRoles();
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getHttpMethod() {
+        return httpMethod;
+    }
+
+    public String[] getRoles() {
+        return roles;
+    }
+
+    public static String getDefaultHttpMethod() {
+        return DEFAULT_HTTP_METHOD;
+    }
+
+    @Override
+    public String toString() {
+        return "AuthorizedUrlPermission{" + "url='" + url + '\'' + ", httpMethod='" + httpMethod + '\'' + ", roles="
+                + Arrays.toString(roles) + '}';
+    }
+
+}
