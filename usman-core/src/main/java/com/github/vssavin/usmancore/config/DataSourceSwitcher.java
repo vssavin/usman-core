2a1f5394e260756b0b75c9675576fe91049e87fc
vssavin <savin1801@gmail.com>
2023-11-28 14:02:03 +0300

    added classes for switching data sources

---
+++ usman-core/src/main/java/com/github/vssavin/usmancore/config/DataSourceSwitcher.java
@@ -0,0 +1,51 @@
+package com.github.vssavin.usmancore.config;
+
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.util.Deque;
+import java.util.NoSuchElementException;
+import java.util.concurrent.ConcurrentLinkedDeque;
+
+/**
+ * Wrapper to provide switching between user management datasource and main datasource.
+ *
+ * @author vssavin on 28.11.2023.
+ */
+@Component
+public class DataSourceSwitcher {
+
+    private final AbstractRoutingDataSource routingDataSource;
+
+    private final Deque<RoutingDataSource.DATASOURCE_TYPE> datasourceStack = new ConcurrentLinkedDeque<>();
+
+    public DataSourceSwitcher(AbstractRoutingDataSource routingDataSource) {
+        this.routingDataSource = routingDataSource;
+    }
+
+    public void switchToUmDataSource() {
+        datasourceStack.push(((RoutingDataSource) routingDataSource).getDatasourceKey());
+        ((RoutingDataSource) routingDataSource).setKey(RoutingDataSource.DATASOURCE_TYPE.UM_DATASOURCE);
+    }
+
+    public void switchToApplicationDataSource() {
+        datasourceStack.push(((RoutingDataSource) routingDataSource).getDatasourceKey());
+        ((RoutingDataSource) routingDataSource).setKey(RoutingDataSource.DATASOURCE_TYPE.APPLICATION_DATASOURCE);
+    }
+
+    public void switchToPreviousDataSource() {
+        try {
+            RoutingDataSource.DATASOURCE_TYPE dsType = datasourceStack.pop();
+            ((RoutingDataSource) routingDataSource).setKey(dsType);
+        }
+        catch (NoSuchElementException e) {
+            ((RoutingDataSource) routingDataSource).setKey(RoutingDataSource.DATASOURCE_TYPE.APPLICATION_DATASOURCE);
+        }
+    }
+
+    public DataSource getCurrentDataSource() {
+        return ((RoutingDataSource) routingDataSource).determineTargetDataSource();
+    }
+
+}
---
+++ usman-core/src/main/java/com/github/vssavin/usmancore/config/RoutingDataSource.java
@@ -0,0 +1,54 @@
+package com.github.vssavin.usmancore.config;
+
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+import org.springframework.lang.NonNull;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Routes connection switching between data sources based on a lookup key.
+ *
+ * @author vssavin on 28.11.2023.
+ */
+class RoutingDataSource extends AbstractRoutingDataSource {
+
+    enum DATASOURCE_TYPE {
+
+        UM_DATASOURCE, APPLICATION_DATASOURCE
+
+    }
+
+    private DATASOURCE_TYPE datasourceKey = DATASOURCE_TYPE.APPLICATION_DATASOURCE;
+
+    private final Map<Object, Object> dataSources = new HashMap<>();
+
+    void setKey(DATASOURCE_TYPE key) {
+        this.datasourceKey = key;
+    }
+
+    public DATASOURCE_TYPE getDatasourceKey() {
+        return datasourceKey;
+    }
+
+    RoutingDataSource() {
+        setTargetDataSources(dataSources);
+    }
+
+    void addDataSource(DATASOURCE_TYPE key, DataSource dataSource) {
+        dataSources.put(key, dataSource);
+    }
+
+    @Override
+    protected Object determineCurrentLookupKey() {
+        return datasourceKey;
+    }
+
+    @NonNull
+    @Override
+    protected DataSource determineTargetDataSource() {
+        return (DataSource) dataSources.get(datasourceKey);
+    }
+
+}
