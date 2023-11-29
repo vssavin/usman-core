8f4638d4c9edb9792e4a64f60cab63e2ce71d119
svs-devel <example@example.com>
2023-11-29 21:13:33 +0300

    spring5: starts processing args for each bean after it is created

---
+++ usman-core-spring5/src/main/java/com/github/vssavin/usmancore/spring5/config/ArgumentsHandlerStarter.java
@@ -0,0 +1,29 @@
+package com.github.vssavin.usmancore.spring5.config;
+
+import com.github.vssavin.usmancore.config.AbstractApplicationArgumentsHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+/**
+ * Ensures that processing of application arguments for each component begins after it is created.
+ *
+ * @author vssavin on 29.11.2023
+ */
+@Configuration
+public class ArgumentsHandlerStarter {
+
+    private final List<AbstractApplicationArgumentsHandler> argumentsHandlerList;
+
+    @Autowired
+    public ArgumentsHandlerStarter(List<AbstractApplicationArgumentsHandler> argumentsHandlerList) {
+        this.argumentsHandlerList = argumentsHandlerList;
+    }
+
+    @PostConstruct
+    private void processArgs() {
+        argumentsHandlerList.forEach(AbstractApplicationArgumentsHandler::processArgs);
+    }
+}
