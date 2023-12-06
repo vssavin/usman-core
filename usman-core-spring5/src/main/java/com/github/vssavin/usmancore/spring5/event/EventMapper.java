547a26a220b83c8b6eb1f873ee978a2b26ee77c8
vssavin <savin1801@gmail.com>
2023-12-06 16:36:06 +0300

    added event entity and mapper

---
+++ usman-core-spring5/src/main/java/com/github/vssavin/usmancore/spring5/event/EventMapper.java
@@ -0,0 +1,25 @@
+package com.github.vssavin.usmancore.spring5.event;
+
+import com.github.vssavin.usmancore.event.EventDto;
+import com.github.vssavin.usmancore.spring5.user.UserMapper;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+/**
+ * Mapper to convert between event entity and event dto.
+ *
+ * @author vssavin on 06.12.2023.
+ */
+@Mapper(componentModel = "spring")
+interface EventMapper {
+
+    UserMapper userMapper = Mappers.getMapper(UserMapper.class);
+
+    @Mapping(target = "user", expression = "java(userMapper.toDto(event.getUser()))")
+    EventDto toDto(Event event);
+
+    @Mapping(target = "user", expression = "java(userMapper.toEntity(eventDto.getUser()))")
+    Event toEntity(EventDto eventDto);
+
+}
