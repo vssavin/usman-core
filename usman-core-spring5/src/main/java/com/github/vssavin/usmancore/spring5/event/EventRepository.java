2757544c7ac9a819d128fb7352c1fb74e16251e5
vssavin <savin1801@gmail.com>
2023-12-07 12:25:06 +0300

    added EventService, EventRepository

---
+++ usman-core-spring5/src/main/java/com/github/vssavin/usmancore/spring5/event/EventRepository.java
@@ -0,0 +1,23 @@
+package com.github.vssavin.usmancore.spring5.event;
+
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * Main repository of event management.
+ *
+ * @author vssavin on 07.12.2023.
+ */
+@Repository
+public interface EventRepository extends PagingAndSortingRepository<Event, Long>, QuerydslPredicateExecutor<Event> {
+
+    @Transactional
+    @NonNull
+    List<Event> findAll();
+
+}
